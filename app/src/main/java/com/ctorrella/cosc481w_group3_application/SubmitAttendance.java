7e81dcd75f7e20ca551134aed94c716c4d9de1f1
BrendanSchmidt <BrendanSchmidt15@yahoo.com>
2020-10-24 14:05:39 -0400

    Added Logout Button and Method in SubmitAttendance View.

--- app/src/main/java/com/ctorrella/cosc481w_group3_application/SubmitAttendance.java
+++ app/src/main/java/com/ctorrella/cosc481w_group3_application/SubmitAttendance.java
@@ -2,13 +2,25 @@ package com.ctorrella.cosc481w_group3_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SubmitAttendance extends AppCompatActivity {
+
+    //Could we add a logout button on this page?
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_attendance);
+    }
+
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        finish();
     }
 }
