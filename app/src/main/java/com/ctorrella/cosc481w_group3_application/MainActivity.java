cbc12e70d549298c517d8e4ca0d61170cb1d8a3b
BrendanSchmidt <BrendanSchmidt15@yahoo.com>
2020-11-30 16:15:10 -0500

    For real this time.

--- app/src/main/java/com/ctorrella/cosc481w_group3_application/MainActivity.java
+++ app/src/main/java/com/ctorrella/cosc481w_group3_application/MainActivity.java
@@ -39,8 +39,6 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     //Initialize variables
-    SupportMapFragment supportMapFragment;
-    FusedLocationProviderClient client;
     FirebaseAuth fAuth;
 
     @Override
@@ -53,25 +51,6 @@ public class MainActivity extends AppCompatActivity {
         EditText usernameEditText = findViewById(R.id.usernameEditText);
         EditText passwordEditText = findViewById(R.id.passwordEditText);
         Button loginButton = findViewById(R.id.loginButton);
-        
-         //Assign variables
-        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.google_map);
-
-        //Initialize fused location
-        client = LocationServices.getFusedLocationProviderClient(this);
-
-        //Check permission
-        if(ActivityCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            //When permission is granted call getCurrentLocation method
-            getCurrentLocation();
-        }else{
-            //When permission is denied
-            //Request permission
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
-        }
 
         fAuth = FirebaseAuth.getInstance();
         if(fAuth.getCurrentUser() != null){ //Check if the user is already logged in.
@@ -104,47 +83,6 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(listener);
 
     }
-    
-     private void getCurrentLocation() {
-        //Initialize task location
-        Task<Location> task = client.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(final Location location) {
-                //When success
-                if(location != null){
-                    //Sync map
-                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-                        @Override
-                        public void onMapReady(GoogleMap googleMap) {
-                            //Initialize latitude and longitude
-                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
-                            //Create marker options
-                            MarkerOptions options = new MarkerOptions().position(latLng)
-                                    .title("I am here");
-                            //Zoom map
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
-                            //Add marker on map
-                            googleMap.addMarker(options);
-                        }
-                    });
-                }
-
-            }
-        });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if( requestCode == 44){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                //When permission is granted, call method
-                getCurrentLocation();
-            }
-
-        }
-    }
-    
 
     public void login(String email, String password, Intent intent)
     {
